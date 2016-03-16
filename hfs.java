05f75ae11cf131fc3d1f3559e25e36e506fe9209
Add Chan <addison95132@gmail.com>
2016-03-16 13:28:49 -0700

    Update hfs.java

--- hfs.java
+++ hfs.java
@@ -8,10 +8,6 @@ public class DigitSum {
     int number = 112345; //insert number here
     int count = 1;
     
-    /**while(number > 9) {
-      number = number / 10;
-      count = count + 1;
-    }*/
     while (number > 0) {
     	count = count + number % 10;
     	number = number / 10;
